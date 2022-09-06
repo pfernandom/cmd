2bc864e4bc9ad3a681cf93c223b773c24588309d
Pedro Soto <psoto@psoto-mn1.linkedin.biz>
2022-09-06 10:05:20 -0700

    Remove pattern temporarily

--- src/cmd/cmd_add.rs
+++ src/cmd/cmd_add.rs
@@ -5,7 +5,10 @@ pub fn add_command(pattern: bool, execute: bool, deps: &Deps) -> Result<(), CmdE
     let note = deps.input.get_input(Some("Write your command".into()));
     print!("{}", note);
 
-    match !pattern || execute {
+    if pattern {
+    }
+
+    match execute {
         true => {
             deps.os.execute_command(&note)?;
             mem.new_command(note)
